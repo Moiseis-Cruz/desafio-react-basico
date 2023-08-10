8c12e2f565f158516553d566c1313cd6a562713a
moizzito@gmail.com <moizzito@gmail.com>
2023-08-09 21:52:46 -0300

    Criei um componente classe Paragraph.

--- src/components/paragraph/paragraph.js
+++ src/components/paragraph/paragraph.js
@@ -1,9 +1,12 @@
+import React from 'react'
 import './paragraph.css'
 
-const Paragraph = () => {
-    return (
-        <p className='text' style={{backgroundColor: '#ab1ab3', textTransform: 'uppercase', color: "#ffffff"}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat adipisci, fuga ab, excepturi eum pariatur error voluptates quo omnis possimus distinctio provident? Quis aperiam inventore nam obcaecati, reiciendis recusandae! Incidunt?</p>
-    )
+class Paragraph extends React.Component {
+    render(){
+        return (
+            <p className='text' style={{backgroundColor: '#ab1ab3', textTransform: 'uppercase', color: "#ffffff"}}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat adipisci, fuga ab, excepturi eum pariatur error voluptates quo omnis possimus distinctio provident? Quis aperiam inventore nam obcaecati, reiciendis recusandae! Incidunt?</p>
+        )
+    }
 }
 
 export default Paragraph
